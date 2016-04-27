844d57d6dc2687c7554c99a47220cb876f846b09
Charlie Erwall <charlie.erwall@gmail.com>
2016-04-27 11:28:57 +0200

    Clean upp regulator header and source
    Also add the common idiom for avoiding name mangling in the header. This is to prepare for inclusion by files compiled with C++

--- inc/regulator.h
+++ inc/regulator.h
@@ -1,51 +1,57 @@
 #ifndef REGULATOR_H_
 #define REGULATOR_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdlib.h>
 #include <pthread.h>
 
 typedef struct regul_t {
+	pthread_mutex_t *mutex;
 	double pitch_ref;
 	double yaw_ref;
-	pthread_mutex_t *mutex;
 } regul_t;
 
 typedef struct data_t {
-	/*Idea for buffer:
-	Double vector of predetermined size. Put u1, u2, y1, y2 , then rinse and repeat.
-	Keep track of number of data points. If there are zero data point then its empty and we cant plot
-	No plan to handle overflow, just make it big enuff, wont take much memory anyway.
+	/* Idea for buffer:
+	Double vector of predetermined size. Put u1, u2, y1, y2 , then rinse and
+	repeat.	Keep track of number of data points. If there are zero data
+	points then it's empty and we cant plot. No plan to handle overflow,
+	just make it big enough, wont take much memory anyway.
 	*/
-	double* buffer;
 	pthread_mutex_t *mutex;
 	int nbr_of_datapoints;
+	double* buffer;
 
 
 } data_t;
 
-
-/**
-Makes sure u is within it bounds
-*/
+/* Makes sure u is within it bounds */
 double limit(double u);
 
-void write_data(double u_pitch,double u_yawp,double y_picth,double y_yaw,data_t *data_buffer);
+/* Write data to buffer in data struct */
+void write_data(double u_pitch, double u_yaw, double y_picth, double y_yaw,
+		data_t *data_buffer);
 
-/**
-Method run by the thread
-*/
-
+/* Method run by the thread */
 void* run_regul(void *regul);
 
-
+/* Initialize regul_t struct. Allocates memory for struct and for members */
 regul_t* init_regul();
 
-void destroy_regul(regul_t *);
+/* Free regul_t members and struct */
+void free_regul(regul_t *regul);
 
+/* Initialize data_t struct. Allocates memory for struct and members */
 data_t* init_data();
 
-void destroy_data(data_t *);
+/* Free data_t members and struct */
+void free_data(data_t *data);
 
-
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* REGULATOR_H_ */
--- src/regulator.c
+++ src/regulator.c
@@ -9,71 +9,66 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-
-
-
-
 void* run_regul(void *input_structs_temp)
 {
-	/*Initialization*/
 	void **struct_pointer = (void **) input_structs_temp; 
 	regul_t *regul = (regul_t *) *struct_pointer ;
 	data_t *data_buffer = (data_t *) *(struct_pointer+1);
-
-
-
-
-
-
 
 	clock_t begin, end;
 	
 	double u_pitch, u_yaw, y_pitch, y_yaw;
 	double h = 2; //Sample time [s] of the regulator. Set better value.
 	while(1) {
-		begin = clock(); //TODO this shouldnt be here, if we start late the next will be late to
-		//TODO if on	
-		/*
-		Switch for read_data() from the input here later.
-		*/
+		/* TODO
+		 * This shouldnt be here, if we start late the next will be
+		 * late too
+		 */
+		begin = clock();
+
+		/* TODO
+		 * Check if regulator is on before proceeding.
+		 */
+
+		/* TODO
+		 * Switch for read_data() from the input here later.
+		 */
+
 		pthread_mutex_lock(regul->mutex);
-		/*
-		Control algorithm
-		Read output
-		*/
+
+		/* Control algorithm and write output */
 		y_pitch = 0;
 		y_yaw = 0;
-		/*
-		Second Kalman
-		read reference (Trajoctory planing might deal with incremental reference changdes)
-		Calculate output (LQG/MPC)
-		*/
+
+		/* Second Kalman
+		 * Read reference (Trajectory planning might deal with
+		 * incremental reference changes). Calculate output (LQG/MPC)
+		 */
 		u_pitch = 0;
 		u_yaw = 0;
 		u_pitch = limit(u_pitch); //Make u somewhere
 		u_yaw= limit(u_yaw);
-		/*
-		Push output
-		Kalman 1;
-		trajectory planning
-		*/
+
+		/* Push output, Kalman 1 and trajectory planning */
+
 		pthread_mutex_unlock(regul->mutex);
 
-		//write to buffer. Lock buffer mutex
+		/* Write to buffer. Lock buffer mutex */
 		pthread_mutex_lock(data_buffer->mutex);
 		write_data(u_pitch,u_yaw,y_pitch,y_yaw,data_buffer);
 		pthread_mutex_unlock(data_buffer->mutex);
 
 
-		/*Handle sleep */
+		/* Handle sleep */
 		end = clock();
 
-		unsigned int sleep_time = 1000000*(h-(double)(end-begin)/CLOCKS_PER_SEC);
-		if (sleep_time <0) {
+		unsigned long int sleep_time = 1000000 * (
+				h - (double)(end-begin) / CLOCKS_PER_SEC);
+		if (sleep_time < 0) {
 			sleep_time = 0;
 		}
 		int stuff = usleep(sleep_time);
-		if (stuff<0) {
+		if (stuff < 0) {
 			printf("usleep failed. Exiting.\n");
 			return NULL;
 		} else {
@@ -86,8 +81,8 @@ void* run_regul(void *input_structs_temp)
 regul_t* init_regul()
 {
 	regul_t *regul = (regul_t*) malloc(sizeof(regul_t));
-	regul->mutex = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_init(regul->mutex,NULL);
+	regul->mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(regul->mutex, NULL);
 	return regul;
 }
 
@@ -98,17 +93,17 @@ void destroy_regul(regul_t *regul)
 	free(regul);
 }
 
-data_t* init_data()
+data_t *init_data()
 {
-	data_t *data = (data_t *) malloc(sizeof(data_t));
-	data->buffer = (double *) malloc(sizeof(double)*4*100);
-	data->mutex = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_init(data->mutex,NULL);
+	data_t *data = (data_t*) malloc(sizeof(data_t));
+	data->buffer = (double*) malloc(400 * sizeof(double));
+	data->mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(data->mutex, NULL);
 	data->nbr_of_datapoints = 0;
 	return data;
 }
 
-void destroy_data(data_t* data)
+void destroy_data(data_t *data)
 {
 	free(data->buffer);
 	pthread_mutex_destroy(data->mutex);
@@ -116,25 +111,27 @@ void destroy_data(data_t* data)
 	free(data);
 }
 
-void add_data(data_t *data) {
-	
+void add_data(data_t *data)
+{
+	;
 }
-/*
-Limits the control signal.
-*/
-double limit(double u) {
-	if (u > 10.0) {
+
+double limit(double u)
+{
+	if (u > 10.0)
 		u = 10.0;
-	} else if (u < -10.0) {
+	else if (u < -10.0)
 		u = -10.0;
-	}
+
 	return u;
 }
 
-void write_data(double u_pitch,double u_yaw,double y_pitch,double y_yaw,data_t* data_buffer) {
-	*((data_buffer->buffer)+data_buffer->nbr_of_datapoints*4) = u_pitch;
-	*((data_buffer->buffer)+data_buffer->nbr_of_datapoints*4+1) = u_yaw;
-	*((data_buffer->buffer)+data_buffer->nbr_of_datapoints*4+2) = y_pitch;
-	*((data_buffer->buffer)+data_buffer->nbr_of_datapoints*4+3) = y_yaw;
-	data_buffer->nbr_of_datapoints = data_buffer->nbr_of_datapoints +1;
+void write_data(double u_pitch, double u_yaw, double y_pitch, double y_yaw,
+		data_t* data)
+{
+	*((data->buffer) + data->nbr_of_datapoints * 4) = u_pitch;
+	*((data->buffer) + data->nbr_of_datapoints * 4 + 1) = u_yaw;
+	*((data->buffer) + data->nbr_of_datapoints * 4 + 2) = y_pitch;
+	*((data->buffer) + data->nbr_of_datapoints * 4 + 3) = y_yaw;
+	data->nbr_of_datapoints += 1;
 }
